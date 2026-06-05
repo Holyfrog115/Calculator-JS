3e0ae2c679fe688e388394db5e792363c6839588
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-06-05 11:16:48 +0700

    Add operate function that calls right function for the operator

--- javascript.js
+++ javascript.js
@@ -23,3 +23,27 @@ function divide(a, b) {
 }
 
 
+function operate(aNumber, bNumber, operator) {
+    // Determines what function to call for answer
+
+    let result = 0;
+
+    switch(operator) {
+        case '+':
+            result = add(aNumber, bNumber);
+            break;
+        case '-':
+            result = subtract(aNumber, bNumber);
+            break;
+        case '*':
+            result = multiply(aNumber, bNumber);
+            break;
+        case '/':
+            result = divide(aNumber, bNumber);
+            break;
+        default:
+            console.log("Unknown operator");
+    }
+
+    return result;
+}
