483f063a1c1594adb3d5c28bb5b51b52cdf58ed1
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-06-10 09:46:40 +0700

    Move update functions to make code more readable

--- javascript.js
+++ javascript.js
@@ -49,39 +49,6 @@ function operate(aNumber, bNumber, operator) {
 }
 
 
-function updateOperationDisplay() {
-    const operation = document.querySelector('.operation');
-    operation.textContent = aNumber + ' ' + operator + ' ' + bNumber;
-    operation.scrollLeft = operation.scrollWidth;
-}
-
-
-function updateHistoryDisplay(answer) {
-    const history = document.querySelector('.history-display');
-
-    const result = document.createElement('div');
-    result.classList.add('result');
-
-    const expression = document.createElement('span');
-    expression.classList.add('expression')
-    expression.textContent = aNumber + operator + bNumber;
-
-    const equals = document.createElement('span');
-    equals.classList.add('equals')
-    equals.textContent = "=";
-
-    const ans = document.createElement('span');
-    ans.classList.add('answer');
-    ans.textContent = answer;
-
-    result.appendChild(expression);
-    result.appendChild(equals);
-    result.appendChild(ans);
-
-    history.appendChild(result);
-}
-
-
 function digitButtons() {
     const btns = document.querySelector(".buttons-display");
     btns.addEventListener("click", (event) => {
@@ -149,6 +116,39 @@ function otherButtons() {
             updateOperationDisplay();
         }
     });
+}
+
+
+function updateOperationDisplay() {
+    const operation = document.querySelector('.operation');
+    operation.textContent = aNumber + ' ' + operator + ' ' + bNumber;
+    operation.scrollLeft = operation.scrollWidth;
+}
+
+
+function updateHistoryDisplay(answer) {
+    const history = document.querySelector('.history-display');
+
+    const result = document.createElement('div');
+    result.classList.add('result');
+
+    const expression = document.createElement('span');
+    expression.classList.add('expression')
+    expression.textContent = aNumber + operator + bNumber;
+
+    const equals = document.createElement('span');
+    equals.classList.add('equals')
+    equals.textContent = "=";
+
+    const ans = document.createElement('span');
+    ans.classList.add('answer');
+    ans.textContent = answer;
+
+    result.appendChild(expression);
+    result.appendChild(equals);
+    result.appendChild(ans);
+
+    history.appendChild(result);
 }
 
 
